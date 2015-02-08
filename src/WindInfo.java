f8c3877216a76b4d67f1803bba049ccf4443b002
satheaki <akshaysathe4@gmail.com>
2015-02-07 23:58:58 -0600

    Create WindInfo.java

---
+++ src/WindInfo.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.example.weatherapp;
+
+/**
+ * @author Akshay
+ * 
+ * Class Containing weather parameter for wind speed
+ *
+ */
+public class WindInfo {
+	double speed, deg;
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public double getDeg() {
+		return deg;
+	}
+
+	public void setDeg(double deg) {
+		this.deg = deg;
+	}
+
+}
